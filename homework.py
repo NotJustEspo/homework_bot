99a068515b034e301b5e9a38bd37c12899b00e3d
Espo <notjustespo@yandex.ru>
2023-11-09 13:54:11 +0300

    Added check_tokens and send_message functions

--- homework.py
+++ homework.py
@@ -1,11 +1,19 @@
-...
+import logging
+import os
+
+import requests
+
+from telegram import Bot
+import time
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('SECRET_PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('SECRET_TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('SECRET_TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -18,27 +26,61 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='program.log',
+    filemode='w',
+    format='%(asctime)s [%(levelname)s] %(message)s'
+)
+
 
 def check_tokens():
-    ...
+    """
+    Проверка обязательных переменных окружения
+    """
+    TOKENS = {
+        'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
+        'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
+        'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID,
+    }
+    for name, token in TOKENS.items():
+        if not token:
+            logging.critical(
+                f"Отсутствует обязательная переменная окружения: '{name}'"
+            )
+            raise SystemExit('Ошибка проверки переменных окружения')
+    logger.debug('Проверка переменных окружения пройдена успешно')
 
 
 def send_message(bot, message):
-    ...
+    """
+    Отправление сообщения в Telegram чат
+    """
+    bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(timestamp):
-    ...
+    """
+    Запрос к единственному эндпоинту API-сервиса
+    """
+    pass
 
 
 def check_response(response):
-    ...
+    """
+    Проверка ответа API на соответствие документации
+    """
+    pass
 
 
 def parse_status(homework):
-    ...
+    """
+    Извлечение информации о конкретной домашней работе (статус)
+    """
+    pass
 
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    # return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
@@ -46,20 +88,16 @@ def main():
 
     ...
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    bot = Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
-
+    check_tokens()
     ...
 
     while True:
         try:
-
-            ...
-
+            check_tokens()
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
-        ...
 
 
 if __name__ == '__main__':
